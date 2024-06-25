61bc83db7f3fad15a2c827da08da65b05d110e7e
karampb06 <270304617@yoobeestudent.ac.nz>
2024-06-26 03:52:31 +0530

    karamjeetsingh

---
+++ ordermain.cpp
@@ -0,0 +1,9 @@
+// ordermain.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "Headerorder.h"
+
+int main() {
+    createOrder(); 
+    return 0;
+}
