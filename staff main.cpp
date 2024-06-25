7486869a21dfb6f9dc4abf1847a86df614da8d77
karampb06 <270304617@yoobeestudent.ac.nz>
2024-06-26 03:16:46 +0530

    Update staff main.cpp

--- staff main.cpp
+++ staff main.cpp
@@ -1,14 +1,11 @@
 // staff main.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#include "Headerstaff.h"
 
+// Include the header file for staff functions and structures
 
-
+#include "Headerstaff.h"  
 using namespace std;
-
-
-
 int main() {
-	displayHomeScreen();
-	return 0;
+    // Display the home screen and handle user interactions
+    displayHomeScreen();
+    return 0;  // Return 0 to indicate successful program termination
 }
