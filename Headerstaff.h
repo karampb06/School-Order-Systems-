d4b8f21442327377244f4596d1c44a826611b3b7
karampb06 <270304617@yoobeestudent.ac.nz>
2024-06-26 03:10:18 +0530

    Update Headerstaff.h

--- Headerstaff.h
+++ Headerstaff.h
@@ -3,12 +3,9 @@
 #ifndef Headerstaff_h
 #define Headerstaff_h
 
-
-
 #include <string>
-
 using namespace std;
-
+// Define a struct to hold staff information
 struct staffInfo {
 	string FullName;
 	string Gender;
@@ -18,13 +15,13 @@ struct staffInfo {
 	string Username;
 	string Password;
 };
-
-
-
+// Function to register a new staff member
 void registerstaff();
+// Function to authenticate staff login, returns true if successful
+
 bool loginstafft();
+
+// Function to display the home screen after successful login
 void displayHomeScreen();
-
-
 
 #endif //Headerstafft_h
